0ec7e34e36e9ad445818d796d67236868e40670a
unknown <leducnghia14102003@gmail.com>
2023-03-03 16:38:37 +0700

    up

---
+++ ss5/exercise/products/src/main/java/com/example/products/model/Product.java
@@ -0,0 +1,66 @@
+package com.example.products.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String price;
+    private String status;
+    private String producer;
+
+    public Product(Integer id, String name, String price, String status, String producer) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.status = status;
+        this.producer = producer;
+    }
+
+    public Product() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+}
---
+++ ss5/exercise/products/src/main/java/com/example/products/repository/impl/ProductRepository.java
@@ -0,0 +1,104 @@
+package com.example.products.repository.impl;
+
+import com.example.products.model.Product;
+import com.example.products.repository.IProductRepository;
+import com.example.products.repository.SessionUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductRepository implements IProductRepository {
+    @Override
+    public List<Product> findALl(String name) {
+        Session session = null;
+        List<Product> productList = null;
+        try {
+            session = SessionUtil.sessionFactory.openSession();
+            productList = session.createQuery("from Product where name like concat('%', :name, '%')").setParameter("name", name).getResultList();
+
+        } catch (Exception e) {
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return productList;
+    }
+
+    @Override
+    public Product findById(int id) {
+        return SessionUtil.entityManager.find(Product.class, id);
+    }
+
+    @Override
+    public void save(Product product) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = SessionUtil.sessionFactory.openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
+            session.persist(product);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public void edit(Product product) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = SessionUtil.sessionFactory.openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
+            Product p = findById(product.getId());
+            p.setName(product.getName());
+            p.setPrice(product.getPrice());
+            p.setStatus(product.getStatus());
+            p.setProducer(product.getProducer());
+            session.update(p);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = SessionUtil.sessionFactory.openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
+            Product product = findById(id);
+            session.remove(product);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
