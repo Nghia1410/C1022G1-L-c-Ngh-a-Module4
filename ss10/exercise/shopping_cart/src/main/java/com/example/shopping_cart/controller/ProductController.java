5e8f1eac411e615ec6abf90a3f38061f79fb1962
unknown <leducnghia14102003@gmail.com>
2023-03-09 21:47:53 +0700

    up

---
+++ ss10/exercise/shopping_cart/src/main/java/com/example/shopping_cart/controller/ProductController.java
@@ -0,0 +1,61 @@
+package com.example.shopping_cart.controller;
+
+import com.example.shopping_cart.model.Product;
+import com.example.shopping_cart.service.IProductService;
+import com.example.shopping_cart.util.CartDto;
+import com.example.shopping_cart.util.ProductDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.Optional;
+
+@Controller
+@SessionAttributes("cart")
+public class ProductController {
+
+    @Autowired
+    private IProductService iProductService;
+
+    @ModelAttribute("cart")
+    public CartDto createCart() {
+        return new CartDto();
+    }
+
+    @GetMapping("/cart")
+    public String showList(Model model) {
+        model.addAttribute("listProduct", iProductService.findAll());
+        return "listProduct";
+    }
+
+    @GetMapping("detail/{id}")
+    public String getDetail(@PathVariable int id, Model model) {
+        model.addAttribute("product", iProductService.findById(id).orElse(null));
+        return "detailProduct";
+    }
+
+    @GetMapping("buy/{id}")
+    public String buyProduct(@PathVariable("id") int id, Model model, @ModelAttribute("cart") CartDto cart) {
+        Optional<Product> product = iProductService.findById(id);
+        ProductDto productDto = new ProductDto();
+        BeanUtils.copyProperties(product, productDto);
+        cart.addProduct(productDto);
+        model.addAttribute("listProduct", cart.getProductMap());
+        return "cartShop";
+    }
+
+//    @PostMapping ("/buy/{id}")
+//    public String deleteCart(@PathVariable("id") int id, @ModelAttribute("cart") CartDto cart, Model model) throws ExceptionProduct {
+//        Optional<Product> product = iProductService.findById(id);
+//        ProductDto productDto = new ProductDto();
+//        BeanUtils.copyProperties(product, productDto);
+//        cart.removeProduct(productDto);
+//        model.addAttribute("listProduct", cart.getProductMap());
+//        return "cartShop";
+//    }
+}
