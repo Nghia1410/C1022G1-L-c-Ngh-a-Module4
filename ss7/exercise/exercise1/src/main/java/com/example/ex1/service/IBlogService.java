6efad58465e80fa45be5e66fec7a9788efecb4f0
unknown <leducnghia14102003@gmail.com>
2023-03-14 13:31:46 +0700

    up

--- ss7/exercise/exercise1/src/main/java/com/example/ex1/service/IBlogService.java
+++ ss7/exercise/exercise1/src/main/java/com/example/ex1/service/IBlogService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface IBlogService {
     List<Blog> findAll(String tittle);
+
     Blog findById(int id);
+
     void save(Blog blog);
+
     void edit(Blog blog);
+
     void delete(int id);
 }
